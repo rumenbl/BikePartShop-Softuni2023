26ee5ced9020222a8150aa9aa7ad5797dc8f4a7f
Rumen <megad3luxyt@gmail.com>
2023-11-13 20:19:38 +0200

    init files

---
+++ src/main/java/me/rumenblajev/bikepartshop/BikePartShopApplication.java
@@ -0,0 +1,13 @@
+package me.rumenblajev.bikepartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikePartShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikePartShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/rumenblajev/bikepartshop/BikePartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rumenblajev.bikepartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikePartShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
