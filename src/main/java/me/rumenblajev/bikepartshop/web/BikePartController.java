112fe25f46cb296e1e623c50ac59557714fbd8d7
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-11-13 22:01:57 +0200

    add structure for web controllers

---
+++ src/main/java/me/rumenblajev/bikepartshop/web/BikePartController.java
@@ -0,0 +1,9 @@
+package me.rumenblajev.bikepartshop.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/parts")
+public class BikePartController {
+}
