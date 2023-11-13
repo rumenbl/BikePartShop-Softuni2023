2ec9774b3bf393add5967d6519a42b3aa2323330
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-11-14 00:07:36 +0200

    add model mapper bean to application configuration

---
+++ src/main/java/me/rumenblajev/bikepartshop/config/ApplicationConfiguration.java
@@ -0,0 +1,13 @@
+package me.rumenblajev.bikepartshop.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
