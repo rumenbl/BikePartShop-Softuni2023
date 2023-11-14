f6f31562c35fb90922e6331d8286dfc5f70399c6
rumenbl <48157527+rumenbl@users.noreply.github.com>
2023-11-14 23:01:17 +0200

    add database seeding for categories

--- src/main/java/me/rumenblajev/bikepartshop/init/DBInit.java
+++ src/main/java/me/rumenblajev/bikepartshop/init/DBInit.java
@@ -1,6 +1,7 @@
 package me.rumenblajev.bikepartshop.init;
 
 import lombok.RequiredArgsConstructor;
+import me.rumenblajev.bikepartshop.services.BikePartCategoryService;
 import me.rumenblajev.bikepartshop.services.RoleService;
 import me.rumenblajev.bikepartshop.services.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -11,9 +12,11 @@ import org.springframework.stereotype.Component;
 public class DBInit implements CommandLineRunner {
     private final UserService userService;
     private final RoleService roleService;
+    private final BikePartCategoryService bikePartCategoryService;
     @Override
     public void run(String... args) throws Exception {
         roleService.initRoles();
         userService.initAdminUser();
+        bikePartCategoryService.initCategories();
     }
 }
