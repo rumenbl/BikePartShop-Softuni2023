42562fc255f130852fdead1fd1a9aababb937ad6
Rumen <megad3luxyt@gmail.com>
2023-11-29 18:44:50 +0200

    refactor and fix: make tests for cartservice use mocking instead of database and context reloading -> decreases test runtime

--- src/test/java/me/rumenblajev/bikepartshop/services/CartServiceTest.java
+++ src/test/java/me/rumenblajev/bikepartshop/services/CartServiceTest.java
@@ -2,6 +2,7 @@ package me.rumenblajev.bikepartshop.services;
 
 import me.rumenblajev.bikepartshop.enums.BikePartCategoryEnum;
 import me.rumenblajev.bikepartshop.models.dto.PartCreateDTO;
+import me.rumenblajev.bikepartshop.models.entity.BikePart;
 import me.rumenblajev.bikepartshop.models.entity.Cart;
 import me.rumenblajev.bikepartshop.models.entity.CartItems;
 import me.rumenblajev.bikepartshop.models.entity.User;
@@ -10,109 +11,152 @@ import me.rumenblajev.bikepartshop.repositories.CartRepository;
 import me.rumenblajev.bikepartshop.repositories.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith(MockitoExtension.class)
 class CartServiceTest {
     @Autowired
     private CartService cartService;
-    @Autowired
+    @MockBean
     private CartRepository cartRepository;
-    @Autowired
+    @MockBean
     private UserRepository userRepository;
-    @Autowired
+    @MockBean
     private CartItemsRepository cartItemsRepository;
-    @Autowired
+    @MockBean
     private BikePartService bikePartService;
     @BeforeEach
     void setUp() {
-        PartCreateDTO partCreateDTO = new PartCreateDTO();
-        partCreateDTO.setTitle("title1");
-        partCreateDTO.setPrice(1.0);
-        partCreateDTO.setStock(1);
-        partCreateDTO.setBrand("brand1");
-        partCreateDTO.setCategory(BikePartCategoryEnum.BRAKES);
-        partCreateDTO.setPictureUrl("pictureUrl1");
-        bikePartService.savePart(partCreateDTO);
-        Cart cart = new Cart();
-        cart.setUser(userRepository.findById(1L).get()); // admin user
-        cartRepository.save(cart);
-        CartItems cartItems = new CartItems();
-        cartItems.setCart(cart);
-        cartItems.setPart(bikePartService.findById(1L).get());
-        cartItems.setAmount(1);
-        cartItemsRepository.save(cartItems);
+        var cartItems = new CartItems(new BikePart(), new Cart());
+        when(cartItemsRepository.findById(1L)).thenReturn(Optional.of(cartItems));
+        when(cartRepository.findById(1L)).thenReturn(Optional.of(new Cart()));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(new User()));
+        when(bikePartService.findById(1L)).thenReturn(Optional.of(new BikePart()));
+//        var partCreateDTO = mockPartCreateDto("title1");
+//        bikePartService.savePart(partCreateDTO);
+//        Cart cart = new Cart();
+//        cart.setUser(userRepository.findById(1L).get()); // admin user
+//        cartRepository.save(cart);
+//        CartItems cartItems = new CartItems();
+//        cartItems.setCart(cart);
+//        cartItems.setPart(bikePartService.findById(1L).get());
+//        cartItems.setAmount(1);
+//        cartItemsRepository.save(cartItems);
     }
 
     @Test
     void test_getOpenUserCart_returnsCartWhenGivenValidUser() {
-        User user = userRepository.findById(1L).get(); // admin user
-        Optional<Cart> cart = cartService.getOpenUserCart(user);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(new User()));
+        var user = userRepository.findById(1L).get();
+        when(cartRepository.findByUserAndStatus(user, "open")).thenReturn(Optional.of(new Cart()));
+
+        var cart = cartService.getOpenUserCart(user);
+
         assertTrue(cart.isPresent());
     }
 
     @Test
     void test_getOpenUserCart_returnsNullWhenGivenInvalidUser() {
-        User user = userRepository.findById(1L).get();
-        user.setId(2L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(new User()));
+        var user = userRepository.findById(1L).get();
+        when(cartRepository.findByUserAndStatus(user, "open")).thenReturn(Optional.empty());
+
         Optional<Cart> cart = cartService.getOpenUserCart(user);
+
         assertTrue(cart.isEmpty());
     }
 
     @Test
     void test_getCartContent_returnsContentWhenGivenValidId() {
-        Optional<Cart> cart = cartRepository.findById(1L);
-        assertFalse(cartService.getCartContent(cart.get().getId()).isEmpty());
+        var cartItems = new CartItems(new BikePart(), new Cart());
+        when(cartRepository.findById(1L)).thenReturn(Optional.of(new Cart()));
+        when(cartItemsRepository.findCartItemsByCartId(1L)).thenReturn(List.of(cartItems));
+
+        assertFalse(cartService.getCartContent(1L).isEmpty());
     }
 
     @Test
     void test_save_doesNotThrowExceptionWhenGivenValidParameters() {
-        CartItems cartItems = cartItemsRepository.findById(1L).get();
+        var cartItems = cartItemsRepository.findById(1L).get();
         cartItems.setAmount(2);
-        Cart cart = cartRepository.findById(1L).get();
+        var cart = cartRepository.findById(1L).get();
         cartService.save(cart, cartItems);
         assertEquals(2, cartItemsRepository.findById(1L).get().getAmount());
     }
 
     @Test
     void test_closeUserCart_closesUserCartWhenGivenValidUser() {
-        User user = userRepository.findById(1L).get();
+        var user = userRepository.findById(1L).get();
+        var cart = cartRepository.findById(1L);
+
+        when(cartRepository.findByUserAndStatus(user,"open")).thenReturn(cart);
         cartService.closeUserCart(user);
+
+        verify(cartRepository,times(1)).save(any());
+
         assertEquals("closed", cartRepository.findById(1L).get().getStatus());
     }
 
     @Test
     void test_addPartToCart_incrementsPartAmountWhenGivenExistingPart() {
-        cartService.addPartToCart(userRepository.findById(1L).get(), 1L);
-        assertEquals(2, cartRepository.findById(1L).get().getCartItems().stream().toList().get(0).getAmount());
+        var user = userRepository.findById(1L).get();
+        var cart = new Cart();
+        cart.setId(3L);
+        var cartItems = new CartItems();
+        var part = new BikePart();
+        part.setId(2L);
+        cartItems.setPart(part);
+        cartItems.setAmount(1);
+        cart.setCartItems(Set.of(cartItems));
+        when(cartRepository.findById(3L)).thenReturn(Optional.of(cart));
+        when(bikePartService.findById(2L)).thenReturn(Optional.of(part));
+        when(cartRepository.findByUserAndStatus(user, "open")).thenReturn(Optional.of(cart));
+        when(cartItemsRepository.findByPartIdAndCartId(2L, 3L)).thenReturn(Optional.of(cartItems));
+
+        cartService.addPartToCart(user, 2L);
+
+        assertEquals(2, cartRepository.findById(3L).get().getCartItems().stream().toList().get(0).getAmount());
     }
+
     @Test
-    void test_addPartToCart_incrementsPartAmountWhenGivenExistingCart() {
-        PartCreateDTO partCreateDTO = new PartCreateDTO();
-        partCreateDTO.setTitle("title2");
+    void test_addPartToCart_createsAndSavesNewCartWhenGivenEmptyCart() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(new User()));
+        when(bikePartService.findById(1L)).thenReturn(Optional.of(new BikePart()));
+        var adminUser = userRepository.findById(1L).get();
+        cartService.closeUserCart(adminUser);
+        verify(cartRepository,times(1)).findByUserAndStatus(adminUser,"open");
+
+        cartService.addPartToCart(adminUser, 1L);
+        verify(cartRepository,times(1)).save(any());
+
+        var cart = new Cart();
+        cart.setCartItems(Set.of(new CartItems(), new CartItems()));
+        when(cartService.getOpenUserCart(adminUser)).thenReturn(Optional.of(cart));
+
+        assertEquals(2, cartService.getOpenUserCart(adminUser).get().getCartItems().size());
+    }
+
+    private PartCreateDTO mockPartCreateDto(final String title) {
+        var partCreateDTO = new PartCreateDTO();
+        partCreateDTO.setTitle(title);
         partCreateDTO.setPrice(1.0);
         partCreateDTO.setStock(1);
         partCreateDTO.setBrand("brand1");
         partCreateDTO.setCategory(BikePartCategoryEnum.BRAKES);
         partCreateDTO.setPictureUrl("pictureUrl1");
-        bikePartService.savePart(partCreateDTO);
 
-        cartService.addPartToCart(userRepository.findById(1L).get(), 2L);
-        assertEquals(2, cartRepository.findById(1L).get().getCartItems().size());
-    }
-
-    @Test
-    void test_addPartToCart_createsAndSavesNewCartWhenGivenEmptyCart() {
-        User adminUser = userRepository.findById(1L).get();
-        cartService.closeUserCart(adminUser);
-        cartService.addPartToCart(adminUser, 1L);
-        assertEquals(1, cartService.getOpenUserCart(adminUser).get().getCartItems().size());
+        return partCreateDTO;
     }
 }
